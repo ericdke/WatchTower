50777ec7653c3650d287104c04ed088d5fd4c06b
Eric Dejonckheere <eric@aya.io>
2016-01-18 23:43:54 +0100

    user defaults

--- PasteBoard/AppDelegate.swift
+++ PasteBoard/AppDelegate.swift
@@ -30,13 +30,31 @@ class AppDelegate: NSObject, NSApplicationDelegate, PasteboardWatcherDelegate {
     }
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
+        if let content = NSUserDefaults().objectForKey("CopiedStrings") as? [String:[AnyObject]] {
+            for (copiedString, items) in content {
+                if let bundle = items[0] as? String,
+                    name = items[1] as? String,
+                    date = items[2] as? Int {
+                    let aa = ActiveApp(name: name, bundleID: bundle)
+                    let d = NSDate(timeIntervalSince1970: NSTimeInterval(date))
+                    watcher.copiedStrings.append(CopiedString(date: d, content: copiedString, source: aa))
+                    watcher.knownApps.insert(KnownApp(activeApp: aa))
+                }
+            }
+            watcher.copiedStrings.sortInPlace { $0.date.timeIntervalSince1970 < $1.date.timeIntervalSince1970 }
+        }
+        
         self.watcher.delegate = self
         // Start watching the general pasteboard.
         self.watcher.startPolling()
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+        let strings = NSMutableDictionary()
+        for app in watcher.copiedStrings {
+            strings[app.content] = NSArray(array: [app.source.bundleID, app.source.name, Int(app.date.timeIntervalSince1970)])
+        }
+        NSUserDefaults().setObject(strings, forKey: "CopiedStrings")
     }
 
 
